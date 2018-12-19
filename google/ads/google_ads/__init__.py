a66bbcbe04cb10209255cd9d4a78128654826996
Ben Karl <benrkarl@users.noreply.github.com>
2018-12-19 15:23:15 -0500

    Add testing boilerplate for MetadataInterceptor Add test for MetadataInterceptor._update_client_call_details_metadata method Add test for MetadataInterceptor.intercept_unary_unary method Implement MetadataInterceptor._update_client_call_details_metadata and use it to update client_call_details metadata within the intercept_unary_unary method before calling continuation function Update ChangeLog Bump version to 0.5.1 Move _ClientCallDetails class outside of MetadataInterceptor class

--- google/ads/google_ads/__init__.py
+++ google/ads/google_ads/__init__.py
@@ -18,4 +18,4 @@ import google.ads.google_ads.client
 import google.ads.google_ads.errors
 
 
-VERSION = '0.4.0'
+VERSION = '0.5.1'
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with io.open('README.rst', 'r', encoding='utf-8') as readme_file:
 
 setup(
     name='google-ads',
-    version='0.5.0',
+    version='0.5.1',
     author='Google LLC',
     author_email='googleapis-packages@google.com',
     classifiers=[

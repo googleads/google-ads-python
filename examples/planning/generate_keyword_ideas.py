e945f4d1f6d60ef7de606f9e28c19c6e5e9569d3
Ben Karl <benrkarl@users.noreply.github.com>
2019-03-12 16:21:35 -0400

    Refactor creation of proto string values to separate functions

--- examples/planning/generate_keyword_ideas.py
+++ examples/planning/generate_keyword_ideas.py
@@ -36,25 +36,13 @@ def main(client, customer_id, location_ids, language_id, keywords, page_url,
                                                    version='v1')
     geo_target_constant_service = client.get_service('GeoTargetConstantService',
                                                      version='v1')
-    keyword_protos = []
-    for keyword in keywords:
-        string_val = client.get_type('StringValue')
-        string_val.value = keyword
-        keyword_protos.append(string_val)
-
-    location_protos = []
-    for location_id in location_ids:
-        location = client.get_type('StringValue')
-        location.value = geo_target_constant_service.geo_target_constant_path(
-            location_id)
-        location_protos.append(location)
-
     keyword_plan_network = client.get_type(
         'KeywordPlanNetworkEnum').GOOGLE_SEARCH_AND_PARTNERS
-
-    language_proto = client.get_type('StringValue')
-    language_proto.value = language_constant_service.language_constant_path(
-        language_id)
+    keyword_protos = build_keyword_protos(client, keywords)
+    location_protos = build_geo_target_protos(client, location_ids,
+                                              geo_target_constant_service)
+    language_proto = build_language_proto(client, language_id,
+                                          language_constant_service)
 
     if (not len(keywords) and page_url):
         url_seed = client.get_type('UrlSeed', version='v1')
@@ -79,6 +67,32 @@ def main(client, customer_id, location_ids, language_id, keywords, page_url,
             keyword_and_url_seed=keyword_and_url_seed)
 
 
+def build_keyword_protos(client, keywords):
+    keyword_protos = []
+    for keyword in keywords:
+        string_val = client.get_type('StringValue')
+        string_val.value = keyword
+        keyword_protos.append(string_val)
+    return keyword_protos
+
+
+def build_geo_target_protos(client, location_ids, geo_target_constant_service):
+    location_protos = []
+    for location_id in location_ids:
+        location = client.get_type('StringValue')
+        location.value = geo_target_constant_service.geo_target_constant_path(
+            location_id)
+        location_protos.append(location)
+    return location_protos
+
+
+def build_language_proto(client, language_id, language_constant_service):
+    language_proto = client.get_type('StringValue')
+    language_proto.value = language_constant_service.language_constant_path(
+        language_id)
+    return language_proto
+
+
 if __name__ == '__main__':
     # GoogleAdsClient will read the google-ads.yaml configuration file in the
     # home directory if none is specified.
@@ -90,28 +104,25 @@ if __name__ == '__main__':
     # The following argument(s) should be provided to run the example.
     parser.add_argument('-c', '--customer_id', type=six.text_type,
                         required=True, help='The Google Ads customer ID.')
-
     # For more information on determining location IDs, see:
     parser.add_argument('-k', '--keywords', type=six.text_type, required=False,
                         help='Comma-separated starter keywords')
-
     # https://developers.google.com/adwords/api/docs/appendix/geotargeting.
     parser.add_argument('-l', '--location_ids', type=six.text_type,
                         required=False, help='Comma-separated location criteria '
                                             'IDs')
-
     # A language criterion ID. For example, specify 1000 for English. For more
     # information on determining this value, see the below link:
     # https://developers.google.com/adwords/api/docs/appendix/codes-formats#languages.
     parser.add_argument('-i', '--language_id', type=six.text_type,
                         required=False, help='A language criterion ID')
-
     # Optional: Specify a URL string related to your business to generate ideas.
     parser.add_argument('-p', '--page_url', type=six.text_type, required=False,
                         help='A URL string related to your business')
 
     parser.set_defaults(location_ids=_DEFAULT_LOCATION_ID,
                         language_id=_DEFAULT_LANGUAGE_ID)
+
     args = parser.parse_args()
 
     main(google_ads_client, args.customer_id, args.location_ids.split(','),

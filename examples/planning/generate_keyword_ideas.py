864e66df00af512f7923771dbda72df436871a90
Ben Karl <benrkarl@users.noreply.github.com>
2019-03-12 23:32:24 -0400

    reduce code duplication and clean up default inputs

--- examples/planning/generate_keyword_ideas.py
+++ examples/planning/generate_keyword_ideas.py
@@ -21,7 +21,7 @@ import sys
 from google.ads.google_ads.client import GoogleAdsClient
 
 _DEFAULT_PAGE_SIZE = 100
-_DEFAULT_LOCATION_ID = '1023191' # location ID for New York, NY
+_DEFAULT_LOCATION_IDS = '1023191' # location ID for New York, NY
 _DEFAULT_LANGUAGE_ID = '1000' # language ID for English
 
 
@@ -44,27 +44,25 @@ def main(client, customer_id, location_ids, language_id, keywords, page_url,
     language_proto = build_language_proto(client, language_id,
                                           language_constant_service)
 
+    url_seed = None
+    keyword_seed = None
+    keyword_url_seed = None
+
     if (not len(keywords) and page_url):
         url_seed = client.get_type('UrlSeed', version='v1')
-        url_seed.url.value = str(page_url)
-        keyword_ideas = keyword_plan_idea_service.generate_keyword_ideas(
-            customer_id, language_proto, location_ids, keyword_plan_network,
-            url_seed=url_seed)
+        url_seed.url.value = page_url
     elif (len(keywords) and not page_url):
         keyword_seed = client.get_type('KeywordSeed', version='v1')
         keyword_seed.keywords.extend(keyword_protos)
-        keyword_ideas = keyword_plan_idea_service.generate_keyword_ideas(
-            customer_id, language_proto,
-            location_protos, keyword_plan_network,
-            keyword_seed=keyword_seed)
     else:
-        keyword_and_url_seed = client.get_type('KeywordAndUrlSeed',
-                                               version='v1')
-        keyword_and_url_seed.url.value = page_url
-        keyword_and_url_seed.keywords.extend(keyword_protos)
-        keyword_ideas = keyword_plan_idea_service.generate_keyword_ideas(
-            customer_id, language_id, location_ids, keyword_plan_network,
-            keyword_and_url_seed=keyword_and_url_seed)
+        keyword_url_seed = client.get_type('KeywordAndUrlSeed', version='v1')
+        keyword_url_seed.url.value = page_url
+        keyword_url_seed.keywords.extend(keyword_protos)
+
+    keyword_ideas = keyword_plan_idea_service.generate_keyword_ideas(
+        customer_id, language_proto, location_protos, keyword_plan_network,
+        url_seed=url_seed, keyword_seed=keyword_seed,
+        keyword_and_url_seed=keyword_url_seed)
 
 
 def build_keyword_protos(client, keywords):
@@ -109,22 +107,24 @@ if __name__ == '__main__':
                         help='Comma-separated starter keywords')
     # https://developers.google.com/adwords/api/docs/appendix/geotargeting.
     parser.add_argument('-l', '--location_ids', type=six.text_type,
-                        required=False, help='Comma-separated location criteria '
-                                            'IDs')
+                        required=False, help='Comma-delimited list of location '
+                                             'criteria IDs')
     # A language criterion ID. For example, specify 1000 for English. For more
     # information on determining this value, see the below link:
     # https://developers.google.com/adwords/api/docs/appendix/codes-formats#languages.
     parser.add_argument('-i', '--language_id', type=six.text_type,
-                        required=False, help='A language criterion ID')
+                        required=False, help='Comma-delimited list of language '
+                                             'criterion IDs')
     # Optional: Specify a URL string related to your business to generate ideas.
     parser.add_argument('-p', '--page_url', type=six.text_type, required=False,
                         help='A URL string related to your business')
 
-    parser.set_defaults(location_ids=_DEFAULT_LOCATION_ID,
-                        language_id=_DEFAULT_LANGUAGE_ID)
+    parser.set_defaults(location_ids=_DEFAULT_LOCATION_IDS,
+                        language_id=_DEFAULT_LANGUAGE_ID, keywords='')
 
     args = parser.parse_args()
 
-    main(google_ads_client, args.customer_id, args.location_ids.split(','),
-         args.language_id, args.keywords.split(','), args.page_url,
+    main(google_ads_client, args.customer_id,
+         filter(None, args.location_ids.split(',')), args.language_id,
+         filter(None, args.keywords.split(',')), args.page_url,
          _DEFAULT_PAGE_SIZE)

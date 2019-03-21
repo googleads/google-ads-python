bf360cf03293b5d7d9c79a89cd06a17f9aec6e37
Fabian Bosler <fabianbosler@MacBook-Pro-2.local>
2019-03-21 19:21:16 +0100

    added exmaple for sitelink creation

--- examples/advanced_operations/create_sitelink.py
+++ examples/advanced_operations/create_sitelink.py
@@ -11,10 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Demonstrates how to create a shared list of negative broad match keywords.
-
-Note that the keywords will be attached to the specified campaign.
-"""
+"""Demonstrates how to create a sitelink extension."""
 
 from __future__ import absolute_import
 

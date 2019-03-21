100ec02839ebf27b0736214736eb3300940b15a0
Fabian Bosler <FBosler@gmail.com>
2019-03-21 19:33:31 +0100

    upload with correct email

--- examples/advanced_operations/create_sitelink.py
+++ examples/advanced_operations/create_sitelink.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
         description='Creates sitelink for the specified customer id')
     # The following argument(s) should be provided to run the example.
     parser.add_argument('-c', '--customer_id', type=six.text_type,
-                        required=True, help='The Google Ads customer ID.')
+                        required=True, help='The Google Ads customer ID')
     args = parser.parse_args()
 
     main(google_ads_client, args.customer_id)

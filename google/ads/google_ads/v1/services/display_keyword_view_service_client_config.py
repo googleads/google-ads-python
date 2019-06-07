e425656bed2110b3af42900876d00a9f09ea02e1
Ben Karl <benrkarl@users.noreply.github.com>
2019-06-07 11:53:56 -0400

    Update service-level timeout configuration to set the initial timeout to the upper bound defined by the Google Ads API.
    Fix the idempotency of some service methods to mark some previously
    non_idempotent methods as idempotent.

--- google/ads/google_ads/v1/services/display_keyword_view_service_client_config.py
+++ google/ads/google_ads/v1/services/display_keyword_view_service_client_config.py
@@ -7,13 +7,13 @@ config = {
             },
             "retry_params": {
                 "default": {
-                    "initial_retry_delay_millis": 100,
+                    "initial_retry_delay_millis": 5000,
                     "retry_delay_multiplier": 1.3,
                     "max_retry_delay_millis": 60000,
-                    "initial_rpc_timeout_millis": 20000,
+                    "initial_rpc_timeout_millis": 3600000,
                     "rpc_timeout_multiplier": 1.0,
-                    "max_rpc_timeout_millis": 20000,
-                    "total_timeout_millis": 600000
+                    "max_rpc_timeout_millis": 3600000,
+                    "total_timeout_millis": 3600000
                 }
             },
             "methods": {

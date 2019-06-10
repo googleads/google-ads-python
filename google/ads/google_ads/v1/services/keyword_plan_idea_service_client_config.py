9aca602b98a5309aa2346457159ca454e9545c45
Ben Karl <5302342+BenRKarl@users.noreply.github.com>
2019-06-10 10:58:02 -0400

    Update service-level timeout configuration to set the initial timeout to (#124)

--- google/ads/google_ads/v1/services/keyword_plan_idea_service_client_config.py
+++ google/ads/google_ads/v1/services/keyword_plan_idea_service_client_config.py
@@ -7,19 +7,19 @@ config = {
             },
             "retry_params": {
                 "default": {
-                    "initial_retry_delay_millis": 100,
+                    "initial_retry_delay_millis": 5000,
                     "retry_delay_multiplier": 1.3,
                     "max_retry_delay_millis": 60000,
-                    "initial_rpc_timeout_millis": 20000,
+                    "initial_rpc_timeout_millis": 3600000,
                     "rpc_timeout_multiplier": 1.0,
-                    "max_rpc_timeout_millis": 20000,
-                    "total_timeout_millis": 600000
+                    "max_rpc_timeout_millis": 3600000,
+                    "total_timeout_millis": 3600000
                 }
             },
             "methods": {
                 "GenerateKeywordIdeas": {
                     "timeout_millis": 600000,
-                    "retry_codes_name": "non_idempotent",
+                    "retry_codes_name": "idempotent",
                     "retry_params_name": "default"
                 }
             }

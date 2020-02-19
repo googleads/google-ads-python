5a6258745464ce14d8a8cbee2d6b8ee79cd6612d
David Wihl <davidwihl@google.com>
2020-02-19 01:40:07 +0000

    get_responsive_search_ads golden (also includes missing add_responsive_search_ad example) (#223)
    * added add_responsive_search_ad
    
    * added get_responsive_search_ad

---
+++ examples/basic_operations/add_responsive_search_ad.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This example adds a responsive search ad to a given ad group.
+
+To get ad groups, run get_ad_groups.py.
+"""
+
+
+import argparse
+import sys
+from uuid import uuid4
+
+from google.ads.google_ads.client import GoogleAdsClient
+from google.ads.google_ads.errors import GoogleAdsException
+
+
+def main(client, customer_id, ad_group_id):
+    ad_group_ad_service = client.get_service('AdGroupAdService', version='v2')
+    ad_group_service = client.get_service('AdGroupService', version='v2')
+
+    # Create the ad group ad.
+    ad_group_ad_operation = client.get_type('AdGroupAdOperation', version='v2')
+    ad_group_ad = ad_group_ad_operation.create
+    ad_group_ad.status = client.get_type(
+        'AdGroupAdStatusEnum', version='v2').PAUSED
+    ad_group_ad.ad_group.value = ad_group_service.ad_group_path(
+        customer_id, ad_group_id)
+
+    # Set responsive search ad info.
+    final_url = ad_group_ad.ad.final_urls.add()
+    final_url.value = 'http://www.example.com'
+
+    # Set a pinning to always choose this asset for HEADLINE_1. Pinning is
+    # optional; if no pinning is set, then headlines and descriptions will be
+    # rotated and the ones that perform best will be used more often.
+    pinned_headline = _create_ad_text_asset(
+        client,
+        f'Cruise to Mars #{str(uuid4())[:8]}',
+        client.get_type(
+            'ServedAssetFieldTypeEnum', version='v2').HEADLINE_1)
+
+    ad_group_ad.ad.responsive_search_ad.headlines.extend([
+        pinned_headline,
+        _create_ad_text_asset(client, 'Best Space Cruise Line'),
+        _create_ad_text_asset(client, 'Experience the Stars')])
+    ad_group_ad.ad.responsive_search_ad.descriptions.extend([
+        _create_ad_text_asset(client, 'Buy your tickets now'),
+        _create_ad_text_asset(client, 'Visit the Red Planet')])
+    ad_group_ad.ad.responsive_search_ad.path1.value = 'all-inclusive'
+    ad_group_ad.ad.responsive_search_ad.path2.value = 'deals'
+
+    # Send a request to the server to add a responsive search ad.
+    try:
+        ad_group_ad_response = ad_group_ad_service.mutate_ad_group_ads(
+            customer_id, [ad_group_ad_operation])
+    except GoogleAdsException as ex:
+        print(f'Request with ID "{ex.request_id}" failed with status '
+              f'"{ex.error.code().name}" and includes the following errors:')
+        for error in ex.failure.errors:
+            print(f'\tError with message "{error.message}".')
+            if error.location:
+                for field_path_element in error.location.field_path_elements:
+                    print(f'\t\tOn field: {field_path_element.field_name}')
+        sys.exit(1)
+
+    for result in ad_group_ad_response.results:
+        print(f'Created responsive search ad with resource name '
+              f'"{result.resource_name}".')
+
+
+def _create_ad_text_asset(client, text, pinned_field=None):
+    """Create an AdTextAsset."""
+    ad_text_asset = client.get_type('AdTextAsset', version='v2')
+    ad_text_asset.text.value = text
+    if pinned_field:
+        ad_text_asset.pinned_field = pinned_field
+    return ad_text_asset
+
+
+if __name__ == '__main__':
+    # GoogleAdsClient will read the google-ads.yaml configuration file in the
+    # home directory if none is specified.
+    google_ads_client = GoogleAdsClient.load_from_storage()
+
+    parser = argparse.ArgumentParser(
+        description=('Adds an expanded text ad to the specified ad group ID, '
+                     'for the given customer ID.'))
+    # The following argument(s) should be provided to run the example.
+    parser.add_argument('-c', '--customer_id', type=str,
+                        required=True, help='The Google Ads customer ID.')
+    parser.add_argument('-a', '--ad_group_id', type=str,
+                        required=True, help='The ad group ID.')
+    args = parser.parse_args()
+
+    main(google_ads_client, args.customer_id, args.ad_group_id)
---
+++ examples/basic_operations/get_responsive_search_ads.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This example gets non-removed responsive search ads in a specified ad group.
+
+To add responsive search ads, run basic_operations/add_responsive_search_ad.py.
+To get ad groups, run basic_operations/get_ad_groups.py.
+"""
+
+import argparse
+import sys
+
+from google.ads.google_ads.client import GoogleAdsClient
+from google.ads.google_ads.errors import GoogleAdsException
+
+
+_DEFAULT_PAGE_SIZE = 1000
+
+
+def main(client, customer_id, page_size, ad_group_id=None):
+    ga_service = client.get_service('GoogleAdsService', version='v2')
+
+    query = '''
+        SELECT ad_group.id, ad_group_ad.ad.id,
+        ad_group_ad.ad.responsive_search_ad.headlines,
+        ad_group_ad.ad.responsive_search_ad.descriptions,
+        ad_group_ad.status FROM ad_group_ad
+        WHERE ad_group_ad.ad.type = RESPONSIVE_SEARCH_AD
+        AND ad_group_ad.status != "REMOVED"'''
+
+    if ad_group_id:
+        query = query + f' AND ad_group.id = {ad_group_id}'
+
+    results = ga_service.search(customer_id, query=query, page_size=page_size)
+    aga_status_enum = client.get_type(
+        'AdGroupAdStatusEnum', version='v2').AdGroupAdStatus
+
+    try:
+        one_found = False
+
+        for row in results:
+            one_found = True
+            ad = row.ad_group_ad.ad
+            print(f'Responsive search ad with resource name '
+                  f'"{ad.resource_name}", '
+                  f'status {aga_status_enum.Name(row.ad_group_ad.status)} '
+                  f'was found.')
+            if ad.responsive_search_ad:
+                responsive_search_ad_info = ad.responsive_search_ad
+                print('Headlines:\n{}\n'
+                      'Descriptions:\n{}\n'.format(
+                          '\n'.join(_ad_text_assets_to_strs(
+                              client, responsive_search_ad_info.headlines)),
+                          '\n'.join(_ad_text_assets_to_strs(
+                              client, responsive_search_ad_info.descriptions))))
+            else:
+                print('\tResponsive search ad info was not found.')
+
+        if not one_found:
+            print('No responsive search ads were found.')
+
+    except GoogleAdsException as ex:
+        print(f'Request with ID "{ex.request_id}" failed with status '
+              f'"{ex.error.code().name}" and includes the following errors:')
+        for error in ex.failure.errors:
+            print(f'\tError with message "{error.message}".')
+            if error.location:
+                for field_path_element in error.location.field_path_elements:
+                    print(f'\t\tOn field: {field_path_element.field_name}')
+        sys.exit(1)
+
+
+def _ad_text_assets_to_strs(client, assets):
+    """Converts a list of AdTextAssets to a list of user-friendly strings."""
+    sa_field_type_enum = client.get_type(
+        'ServedAssetFieldTypeEnum', version='v2').ServedAssetFieldType
+    s = []
+    for asset in assets:
+        s.append('\t"' + asset.text.value + '" pinned to ' +
+                 sa_field_type_enum.Name(asset.pinned_field))
+    return s
+
+if __name__ == '__main__':
+    # GoogleAdsClient will read the google-ads.yaml configuration file in the
+    # home directory if none is specified.
+    google_ads_client = GoogleAdsClient.load_from_storage()
+
+    parser = argparse.ArgumentParser(
+        description='List responsive display ads for specified customer. '
+                    'An ad_group is optional.')
+    # The following argument(s) should be provided to run the example.
+    parser.add_argument('-c', '--customer_id', type=str,
+                        required=True, help='The Google Ads customer ID.')
+    parser.add_argument('-a', '--ad_group_id', type=str,
+                        required=False, help='The ad group ID. ')
+    args = parser.parse_args()
+
+    main(google_ads_client, args.customer_id, _DEFAULT_PAGE_SIZE,
+         ad_group_id=args.ad_group_id)

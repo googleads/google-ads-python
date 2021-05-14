99378cee809475c34431473f9a98a1da7da8b5f5
Laura Chevalier <laurachevalier@google.com>
2021-05-14 17:12:26 +0000

    use discover
    Change-Id: Ia2676b22f3076cc5d3973172569240dbbe9ca768

--- noxfile.py
+++ noxfile.py
@@ -10,31 +10,10 @@ def tests(session):
         "pyfakefs>=3.5,<3.6",
         "coverage==5.5",
     )
-    test_modules = [
-        "tests.client_test.GoogleAdsClientTest",
-        "tests.config_test.ConfigTest",
-        "tests.oauth2_test.OAuth2Tests",
-        "tests.util_test.ConvertStringTest",
-        "tests.util_test.SetNestedMessageFieldTest",
-        "tests.util_test.GetNestedMessageFieldTest",
-        "tests.interceptors.exception_interceptor_test.ExceptionInterceptorTest", 
-        "tests.interceptors.interceptor_test.InterceptorTest",
-        "tests.interceptors.logging_interceptor_test.LoggingInterceptorTest",
-        "tests.interceptors.metadata_interceptor_test.MetadataInterceptorTest",
-    ]
-    session.run("coverage", "erase")
-    for module in test_modules:
-        session.run(
-            "coverage",
-            "run",
-            "--append",
-            "-m",
-            "unittest",
-            module,
-        )
+    session.run("coverage", "run", "--append", "-m", "unittest", "discover", "-s=.", "-p", "*_test.py")
     session.run(
         "coverage",
         "report",
         "-m",
-        "--omit=.nox/*"
+        "--omit=.nox/*,examples/*,*/__init__.py"
     )

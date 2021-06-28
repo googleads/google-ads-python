d507e5ec489a5d3266219535cee4c7f4cc8de67d
Laura Chevalier <laurachevalier@google.com>
2021-06-28 17:09:09 +0000

    Add examples for conversion custom variables and run black. Resolves b/191861593.
    Change-Id: I69773ca83ac0acad5526a729a1329028ad84dff7

--- examples/remarketing/upload_offline_conversion.py
+++ examples/remarketing/upload_offline_conversion.py
@@ -35,25 +35,54 @@ def main(
     gclid,
     conversion_date_time,
     conversion_value,
+    conversion_custom_variable_id,
+    conversion_custom_variable_value,
 ):
-    """Creates a click conversion with a default currency of USD."""
+    """Creates a click conversion with a default currency of USD.
+
+    Args:
+        client: An initialized GoogleAdsClient instance.
+        customer_id: The client customer ID string.
+        conversion_action_id: The ID of the conversion action to upload to.
+        gclid: The Google Click Identifier ID.
+        conversion_date_time: The the date and time of the conversion (should be
+            after the click time). The format is 'yyyy-mm-dd hh:mm:ss+|-hh:mm',
+            e.g. '2021-01-01 12:32:45-08:00'.
+        conversion_value: The conversion value in the desired currency.
+        conversion_custom_variable_id: The ID of the conversion custom
+            variable to associate with the upload.
+        conversion_custom_variable_value: The str value of the conversion custom
+            variable to associate with the upload.
+    """
     click_conversion = client.get_type("ClickConversion")
     conversion_action_service = client.get_service("ConversionActionService")
-    click_conversion.conversion_action = conversion_action_service.conversion_action_path(
-        customer_id, conversion_action_id
+    click_conversion.conversion_action = (
+        conversion_action_service.conversion_action_path(
+            customer_id, conversion_action_id
+        )
     )
     click_conversion.gclid = gclid
     click_conversion.conversion_value = float(conversion_value)
     click_conversion.conversion_date_time = conversion_date_time
     click_conversion.currency_code = "USD"
 
+    if conversion_custom_variable_id and conversion_custom_variable_value:
+        conversion_custom_variable = client.get_type("CustomVariable")
+        conversion_custom_variable.conversion_custom_variable = (
+            conversion_custom_variable_id
+        )
+        conversion_custom_variable.value = conversion_custom_variable_value
+        click_conversion.custom_variables.append(conversion_custom_variable)
+
     conversion_upload_service = client.get_service("ConversionUploadService")
     request = client.get_type("UploadClickConversionsRequest")
     request.customer_id = customer_id
     request.conversions = [click_conversion]
     request.partial_failure = True
-    conversion_upload_response = conversion_upload_service.upload_click_conversions(
-        request=request,
+    conversion_upload_response = (
+        conversion_upload_service.upload_click_conversions(
+            request=request,
+        )
     )
     uploaded_click_conversion = conversion_upload_response.results[0]
     print(
@@ -103,7 +132,7 @@ if __name__ == "__main__":
         "--conversion_date_time",
         type=str,
         required=True,
-        help="The the date and time of the "
+        help="The date and time of the "
         "conversion (should be after the click time). The "
         'format is "yyyy-mm-dd hh:mm:ss+|-hh:mm", e.g. '
         "“2019-01-01 12:32:45-08:00”",
@@ -115,6 +144,18 @@ if __name__ == "__main__":
         required=True,
         help="The conversion value.",
     )
+    parser.add_argument(
+        "-w",
+        "--conversion_custom_variable_id",
+        type=str,
+        help="The ID of the conversion custom variable to associate with the upload.",
+    )
+    parser.add_argument(
+        "-x",
+        "--conversion_custom_variable_value",
+        type=str,
+        help="The value of the conversion custom variable to associate with the upload.",
+    )
     args = parser.parse_args()
 
     try:
@@ -125,6 +166,8 @@ if __name__ == "__main__":
             args.gclid,
             args.conversion_date_time,
             args.conversion_value,
+            args.conversion_custom_variable_id,
+            args.conversion_custom_variable_value,
         )
     except GoogleAdsException as ex:
         print(

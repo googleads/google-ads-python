d77073fa14fa9c22309952baf5bfa6a4483ac752
Ben Karl <benkarl@google.com>
2021-06-30 15:55:19 -0400

    Fix package name in base transport files (#462)

--- google/ads/googleads/v6/services/services/keyword_plan_idea_service/transports/base.py
+++ google/ads/googleads/v6/services/services/keyword_plan_idea_service/transports/base.py
@@ -29,9 +29,7 @@ from google.ads.googleads.v6.services.types import keyword_plan_idea_service
 
 try:
     DEFAULT_CLIENT_INFO = gapic_v1.client_info.ClientInfo(
-        gapic_version=pkg_resources.get_distribution(
-            "google-ads-googleads",
-        ).version,
+        gapic_version=pkg_resources.get_distribution("google-ads",).version,
     )
 except pkg_resources.DistributionNotFound:
     DEFAULT_CLIENT_INFO = gapic_v1.client_info.ClientInfo()

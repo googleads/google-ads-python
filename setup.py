e56356002ef8fa68fb204cb9cca0c38540545baa
Ben Karl <benkarl@google.com>
2021-07-07 13:28:15 -0400

    Release version 13.0.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with io.open("README.rst", "r", encoding="utf-8") as readme_file:
 
 setup(
     name="google-ads",
-    version="12.0.0",
+    version="13.0.0",
     author="Google LLC",
     author_email="googleapis-packages@google.com",
     classifiers=[

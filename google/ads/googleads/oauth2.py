fc071d1539594ea5febcc812a65fdf0aca5703c3
Srijan <srijan.chaudhuri@agilebits.com>
2024-06-19 16:04:41 -0400

    removing usage of env variables

--- google/ads/googleads/oauth2.py
+++ google/ads/googleads/oauth2.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 """A set of functions to help initialize OAuth2 credentials."""
 
-import os
 import json
 import functools
 from requests import Session
@@ -81,34 +80,33 @@ def get_installed_app_credentials(
 
 # In environments where it is preferable to supply secrets or other configuration via
 # environment variables or where it is not feasible to make a .json key file available,
-# file available, you may instead provide credential as a JSON string in the
-# `GOOGLE_ADS_SERVICE_ACCOUNT_INFO_STRING` environment variable. This JSON string
+# file available, you may instead provide credential as a JSON string. This JSON string
 # should contain the same contents and be formatted the same way as the contents
 # of a .json key file.
 
 @_initialize_credentials_decorator
 def get_service_account_credentials(
-    json_key_file_path, subject, http_proxy=None, scopes=_SERVICE_ACCOUNT_SCOPES
+    json_key, subject, http_proxy=None, scopes=_SERVICE_ACCOUNT_SCOPES, is_path=True
 ):
     """Creates and returns an instance of oauth2.service_account.Credentials.
 
     Args:
-        json_key_file_path: A str of the path to the private key file location.
+        json_key: A str of the path to the private key file location or str of the JSON key file contents (when is_path=False)
         subject: A str of the email address of the delegated account.
         scopes: A list of additional scopes.
+        is_path: A boolean indicating whether the json_key is a path or a string.
 
     Returns:
         An instance of oauth2.credentials.Credentials
     """
-    service_account_info = os.getenv("GOOGLE_ADS_SERVICE_ACCOUNT_INFO_STRING")
-    if service_account_info:
+    if not is_path:
         return ServiceAccountCreds.from_service_account_info(
-            json.loads(service_account_info), subject=subject, scopes=scopes
+            json.loads(json_key), subject=subject, scopes=scopes
         )
-    return ServiceAccountCreds.from_service_account_file(
-        json_key_file_path, subject=subject, scopes=scopes
-    )
-
+    else:
+        return ServiceAccountCreds.from_service_account_file(
+            json_key, subject=subject, scopes=scopes
+        )
 
 def get_credentials(config_data):
     """Decides which type of credentials to return based on the given config.
@@ -120,8 +118,11 @@ def get_credentials(config_data):
         An initialized credentials instance.
     """
     required_installed_app_keys = config.get_oauth2_installed_app_keys()
-    required_service_account_keys = (
-        config.get_oauth2_required_service_account_keys()
+    required_service_account_path_keys = (
+        config.get_oauth2_required_service_account_path_keys()
+    )
+    required_service_account_string_keys = (
+        config.get_oauth2_required_service_account_string_keys()
     )
 
     if all(key in config_data for key in required_installed_app_keys):
@@ -132,23 +133,28 @@ def get_credentials(config_data):
             config_data.get("refresh_token"),
             http_proxy=config_data.get("http_proxy"),
         )
-    elif "GOOGLE_ADS_SERVICE_ACCOUNT_INFO_STRING" in os.environ:
-        return get_service_account_credentials(
-            None,
-            config_data.get("impersonated_email"),)
-    elif all(key in config_data for key in required_service_account_keys):
-        # Using the Service Account Flow
+    elif all(key in config_data for key in required_service_account_path_keys):
+        # Using the Service Account Path Flow
         return get_service_account_credentials(
             config_data.get("json_key_file_path"),
             config_data.get("impersonated_email"),
             http_proxy=config_data.get("http_proxy"),
         )
+    elif all(key in config_data for key in required_service_account_string_keys):
+        # Using the Service Account String Flow
+        return get_service_account_credentials(
+            config_data.get("json_key_string"),
+            config_data.get("impersonated_email"),
+            http_proxy=config_data.get("http_proxy"),
+            is_path=False,
+        )
     else:
         raise ValueError(
             "Your YAML file is incorrectly configured for OAuth2. "
             "You need to define credentials for either the OAuth2 "
-            "installed application flow ({}) or service account "
-            "flow ({}).".format(
-                required_installed_app_keys, required_service_account_keys
+            "installed application flow ({}) or service account using path"
+            "flow ({}). or service account using string"
+            "flow ({})".format(
+                required_installed_app_keys, required_service_account_path_keys, required_service_account_string_keys
             )
         )

4f8878d9b8be6aa4ecfbbc0659290a5232046956
Srijan <srijan.chaudhuri@agilebits.com>
2024-06-19 16:07:52 -0400

    "reformatting to adhere to config"

--- google/ads/googleads/oauth2.py
+++ google/ads/googleads/oauth2.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """A set of functions to help initialize OAuth2 credentials."""
-
 import json
 import functools
 from requests import Session

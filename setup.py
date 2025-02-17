e4f005e36aadf4a84834e84ccfa3d6a488b831b9
hamsh <hamed.sheykhlu@gmail.com>
2025-02-17 14:51:35 +0300

    python3.13
    add python 3.13 support

--- setup.py
+++ setup.py
@@ -49,10 +49,11 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     description="Client library for the Google Ads API",
     include_package_data=True,
-    python_requires=">=3.8, <3.13",
+    python_requires=">=3.8, <3.14",
     long_description=long_description,
     install_requires=install_requires,
     extras_require={

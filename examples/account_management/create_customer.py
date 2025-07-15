276b8433a7df6e340cdf36b34158c0b858ed5975
Bob Hancock <108475962+bobhancockg@users.noreply.github.com>
2025-07-15 15:01:29 -0400

    Add annotations to examples/account_management (#937)

--- examples/account_management/create_customer.py
+++ examples/account_management/create_customer.py
@@ -27,13 +27,28 @@ from datetime import datetime
 
 from google.ads.googleads.client import GoogleAdsClient
 from google.ads.googleads.errors import GoogleAdsException
+from google.ads.googleads.v20.resources.types.customer import Customer
+from google.ads.googleads.v20.services.services.customer_service.client import (
+    CustomerServiceClient,
+)
+from google.ads.googleads.v20.services.types.customer_service import (
+    CreateCustomerClientResponse,
+)
 
 
 # [START create_customer]
-def main(client, manager_customer_id):
-    customer_service = client.get_service("CustomerService")
-    customer = client.get_type("Customer")
-    now = datetime.today().strftime("%Y%m%d %H:%M:%S")
+def main(client: GoogleAdsClient, manager_customer_id: str) -> None:
+    """The main method that creates all necessary entities for the example.
+
+    Args:
+        client: an initialized GoogleAdsClient instance.
+        manager_customer_id: a manager client customer ID.
+    """
+    customer_service: CustomerServiceClient = client.get_service(
+        "CustomerService"
+    )
+    customer: Customer = client.get_type("Customer")
+    now: str = datetime.today().strftime("%Y%m%d %H:%M:%S")
     customer.descriptive_name = f"Account created with CustomerService on {now}"
     # For a list of valid currency codes and time zones see this documentation:
     # https://developers.google.com/google-ads/api/reference/data/codes-formats
@@ -43,11 +58,13 @@ def main(client, manager_customer_id):
     # options see: https://support.google.com/google-ads/answer/6305348
     customer.tracking_url_template = "{lpurl}?device={device}"
     customer.final_url_suffix = (
-        "keyword={keyword}&matchtype={matchtype}" "&adgroupid={adgroupid}"
+        "keyword={keyword}&matchtype={matchtype}&adgroupid={adgroupid}"
     )
 
-    response = customer_service.create_customer_client(
-        customer_id=manager_customer_id, customer_client=customer
+    response: CreateCustomerClientResponse = (
+        customer_service.create_customer_client(
+            customer_id=manager_customer_id, customer_client=customer
+        )
     )
     print(
         f'Customer created with resource name "{response.resource_name}" '
